df17b88a02bda10f5e1507a6a067dab77eec4d83
Milosz Gembicki <mg1313@live.mdx.ac.uk>
2023-04-22 23:53:23 +0100

    Added service worker to make it downloadable.

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+var cacheName = "classes-v1";
+var cacheFiles = [
+    "index.html",
+    // "subjects.j",
+    "public/character-image.jpg",
+    "landscape.jpg",
+    "mental.jpg"
+];
+self.addEventListener("install", function (e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            //return the file if it is not in the cache
+            if (cachedFile) {
+                console.log(
+                    "[Service Worker] Resource fetched from the cache for: " +
+                    e.request.url
+                );
+                return cachedFile;
+            } else {
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        //add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log(
+                            "[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url
+                        );
+                        return response;
+                    });
+                });
+            }
+        })
+    );
+});
